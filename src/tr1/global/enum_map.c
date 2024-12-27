ffc04be7f0bbd8cdca51de7c8e5ca337f8a9e1f0
Marcin Kurczewski <dash@wind.garden>
2024-12-27 23:17:24 +0100

    enum-map: fix declaring enum maps via libtrx
    This change, though minor, addresses three issues:
    
    - It resolves the issue of client games not including libtrx's
      enum_map.def, which made its content ignored.
    - It removes the redundant INPUT_ROLE manual definitions, as they're
      already provided via the more maintainable input/roles.def macro
      processing.
    - It moves the trx-related macro work to libtrx where it belongs more.

--- src/tr1/global/enum_map.c
+++ src/tr1/global/enum_map.c
@@ -1,6 +1,7 @@
 #include "global/types.h"
 
 #include <libtrx/enum_map.h>
+#include <libtrx/game/gamebuf.h>
 #include <libtrx/game/input.h>
 #include <libtrx/game/objects/ids.h>
 #include <libtrx/screenshot.h>
@@ -8,6 +9,8 @@
 void EnumMap_Init(void)
 {
 #include "global/enum_map.def"
+
+#include <libtrx/game/enum_map.def>
 
 #undef OBJ_ID_DEFINE
 #define OBJ_ID_DEFINE(object_id, value)                                        \
--- src/tr2/global/enum_map.c
+++ src/tr2/global/enum_map.c
@@ -1,6 +1,7 @@
 #include "global/types.h"
 
 #include <libtrx/enum_map.h>
+#include <libtrx/game/gamebuf.h>
 #include <libtrx/game/input.h>
 #include <libtrx/game/objects/ids.h>
 #include <libtrx/gfx/common.h>
@@ -10,13 +11,5 @@ void EnumMap_Init(void)
 {
 #include "global/enum_map.def"
 
-#undef OBJ_ID_DEFINE
-#define OBJ_ID_DEFINE(object_id, value)                                        \
-    EnumMap_Define("GAME_OBJECT_ID", object_id, #object_id);
-#include <libtrx/game/objects/ids.def>
-
-#undef INPUT_ROLE_DEFINE
-#define INPUT_ROLE_DEFINE(role_name, state_name)                               \
-    EnumMap_Define("INPUT_ROLE", INPUT_ROLE_##role_name, #state_name);
-#include <libtrx/game/input/roles.def>
+#include <libtrx/game/enum_map.def>
 }
